ce8c7c8f381847403b42bd9aebdbc873bf6ee6bb
demon_home <762853138@qq.com>
2024-06-08 23:09:39 +0800

    scrapy

--- movie_check/movie_search.py
+++ movie_check/movie_search.py
@@ -6,8 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 def search(key):
