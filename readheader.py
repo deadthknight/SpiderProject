6b0a3d933b1cbe7c8380f5f2df208d1a351126be
demon_home <762853138@qq.com>
2024-06-10 23:09:21 +0800

    infor

--- readheader.py
+++ readheader.py
@@ -18,5 +18,32 @@ def readheaders(file):
 
 
 if __name__ == '__main__':
-    print(readheaders('http_header.txt'))
+    # print(readheaders('http_header.txt'))
+    # lst =['1.a','a.b','1.c']
+    # n = 0
+    # for x in lst:
+    #
+    #     if x.startswith('1'):
+    #         lst[n]='MM'
+    #     n+=1
+    # print(lst)
+    #
+    # for x in range(len(lst)):
+    #     if lst[x].startswith('1'):
+    #         lst[x]='MM'
+    # print(lst)
+    # print(dir(list))
 
+    lst = [11,22,33,44,55,66,77,88,99]
+    result = {}
+    # {'bigger':[55,66,77,88,99],'smaller':[11,22,33,44]}
+    # print()
+    for item in lst:
+        if item > 50:
+            result.setdefault('bigger',[]).append(item)
+        else:
+            result.setdefault('smaller',[]).append(item)
+    print(result)
+    print(result.setdefault('medium'))
+    print(result.setdefault('taller',100))
+    print(result)
