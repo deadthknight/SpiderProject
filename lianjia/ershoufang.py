c627b0b0f5cd8619b5408f67e30fa9bbfd621914
demon_home <762853138@qq.com>
2024-07-11 15:04:11 +0800

    all

---
+++ lianjia/ershoufang.py
@@ -0,0 +1,60 @@
+# ！usr/bin/env Python3.11
+# -*-coding:utf-8 -*-
+import requests
+
+headers = {"User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Mobile Safari/537.36"}
+name = input("请输入小区名称:")
+
+
+def get_source_page(url,page):
+
+    params = {"cityId": 110000,
+              "condition": "%2Fpg"+ str(page)+"rs"+name,
+              "curPage": page}
+    response = requests.get(url,headers=headers,params=params)
+    # print(response.request.url)
+    return response.json()
+def parse_source_page(source_page):
+    total = []
+    list = source_page["data"]["data"]["getErShouFangList"]["list"]
+    for x in list:
+        # title = x.get("title")
+        # desc = x["desc"]
+        # totalPrice = x["totalPrice"]
+        # avgPrice = x["unitPrice"]
+        title = x.get("title", "")
+        desc = x.get("desc", "")
+        totalPrice = x.get("totalPrice", "")
+        avgPrice = x.get("unitPrice", "")
+        # if name in title or name in desc:
+        dic={"小区名称":name,"详细信息":desc,"总价":totalPrice, "单价":avgPrice}
+        total.append(dic)
+    return total
+def main():
+    url = "https://m.lianjia.com/liverpool/api/ershoufang/getList?"
+    page = 1
+    get_all= []
+    while True:
+        source_page = get_source_page(url,page)
+        data = parse_source_page(source_page)
+        get_all.extend(data)
+        if source_page["data"]["data"]["getErShouFangList"]["hasMoreData"]==0:
+            break
+        page += 1
+        # print(page)
+    return get_all
+
+
+
+if __name__ == '__main__':
+   total= main()
+   print(len(total))
+   total = sorted(total,key=lambda x:int(x["总价"].split('万')[0]))
+   # total = set(total)
+   # # total = list(total)
+   # print(type(total))
+   # # print(len(main()))
+   for i in total:
+       print(i)
+
+
