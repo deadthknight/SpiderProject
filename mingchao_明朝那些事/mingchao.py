7375f3be665e2de5d5fb4a650e70b78916b8cac5
demon_home <762853138@qq.com>
2024-07-13 23:09:52 +0800

    all

--- mingchao_明朝那些事/mingchao.py
+++ mingchao_明朝那些事/mingchao.py
@@ -9,13 +9,14 @@ import aiofiles
 import os
 from readheader import readheaders
 
+
 def get_chapter_urls(sourcetext):
     chapter_urls = []
     tree = etree.HTML(sourcetext)
     mulu_list = tree.xpath('//*[@class="mulu"]')
     for mulu in mulu_list:
         chapter_name = mulu.xpath('.//tr[1]//text()')
-        chapter_name = ''.join(chapter_name).strip().replace('：','-')
+        chapter_name = ''.join(chapter_name).strip().replace('：', '-')
         tr_list = mulu.xpath('.//tr')[1:]
         for tr in tr_list:
             td_list = tr.xpath('./td')
@@ -26,33 +27,37 @@ def get_chapter_urls(sourcetext):
                 chapter_name_children_url = ''.join(chapter_name_children_url).strip()
                 if not chapter_name_children:
                     continue
-                dic = {"卷名":chapter_name,
-                       "章名":chapter_name_children,
-                       "url":chapter_name_children_url
+                dic = {"卷名": chapter_name,
+                       "章名": chapter_name_children,
+                       "url": chapter_name_children_url
                        }
                 chapter_urls.append(dic)
     return chapter_urls
 
+
 def get_source_page(url):
-    response = requests.get(url,headers=readheaders('../http_header.txt'),verify=False)
-    # response.encoding = chardet.detect(response.content)['encoding'] #解决乱码 方案一
-    response.encoding = "UTF-8"  #方案二
+    response = requests.get(url, headers=readheaders('../http_header.txt'), verify=False)
+    response.encoding = chardet.detect(response.content)['encoding'] #解决乱码 方案一
+    # response.encoding = "UTF-8"  #方案二
     return response.text
 
-async def download_one(url,file_path):
-    print('下载该文章')
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
-        async with session.get(url=url,headers=readheaders('../http_header.txt')) as response:
+
+async def download_one(url, file_path):
+    # print('下载该文章')
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:  #ssl 报错
+        async with session.get(url=url, headers=readheaders('../http_header.txt')) as response:
             page_source = await response.text(encoding='utf-8')
             tree = etree.HTML(page_source)
-            content = tree.xpath('//div[@class="content"]/p[position() != last()]/text()')
-            content = ''.join(content).replace('\n','').replace('\r','').strip()
-            async with aiofiles.open(file_path,mode='w',encoding='utf-8') as f:
+            content = tree.xpath('//div[@class="content"]/p[position() != last()]/text()')  # 不要最后一个P里面的text
+            content = ''.join(content).replace('\n', '').replace('\r', '').strip()
+            async with aiofiles.open(file_path, mode='w', encoding='utf-8') as f:
                 await f.write(content)
-    print('下载完毕')
+    # print('下载完毕')
+
+
 async def download_file(chapter_list):
-    tasks =[]
-    i =1
+    tasks = []
+    i = 1
     for chapter in chapter_list:
         volume_name = chapter['卷名']
         chapter_name = chapter['章名']
@@ -60,14 +65,11 @@ async def download_file(chapter_list):
 
         if not os.path.exists(f'./{volume_name}'):
             os.makedirs(f'./{volume_name}')
-        filepath = f'./{volume_name}/{str(i)+"_"+chapter_name}.txt'
-        t = asyncio.create_task(download_one(chapter_url,filepath))
+        filepath = f'./{volume_name}/{str(i) + "_" + chapter_name}.txt'
+        t = asyncio.create_task(download_one(chapter_url, filepath))
         tasks.append(t)
-        i+=1
+        i += 1
     await asyncio.wait(tasks)
-        # async with aiohttp.ClientSession() as session:
-        #     async with session.get(url= chapter_url,headers=readheaders('../http_header.txt')) as response:
-
 
 
 def main():
@@ -75,6 +77,8 @@ def main():
     source_page = get_source_page(url)
     chapter_list = get_chapter_urls(source_page)
     asyncio.run(download_file(chapter_list))
+    print('所有文章已下载完毕')
+
 
 if __name__ == '__main__':
     main()
