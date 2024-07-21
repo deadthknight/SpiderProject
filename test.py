5df6db4151f6069fa6a455fdb834aca820b0039e
demon_home <762853138@qq.com>
2024-07-21 10:09:56 +0800

    all

--- test.py
+++ test.py
@@ -22,66 +22,71 @@ b = {4, 5, 6, 7, 8}
 
 # 列表删除,循环删除会存在删不干净，
 
-lst = ['11', '12', '13', '14']
-for x in lst:
-    if x.startswith('1'):
-        lst.remove(x)
+# lst = ['11', '12', '13', '14']
+# for x in lst:
+#     if x.startswith('1'):
+#         lst.remove(x)
 # print(lst)
 # ['12', '14']
 # remove后列表内存会往前移动，所以会出现删不干��的现象
 # 方法一：
-new_lst = []
-for x in lst:
-    if x.startswith('1'):
-        new_lst.append(x)
-for y in new_lst:
-    lst.remove(y)
-# print(lst)
-# []
-# 方法二：
-for x in lst[:]:
-    if x.startswith('1'):
-        lst.remove(x)
-# print(lst)
-
-# 字典删除，循环字典删除，删除数据的数据会报错
-
-dic = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
-# for x in dic:  # dictionary changed size during iteration
+# new_lst = []
+# for x in lst:
+#     if x.startswith('1'):
+#         new_lst.append(x)
+# for y in new_lst:
+#     lst.remove(y)
+# # print(lst)
+# # []
+# # 方法二：
+# for x in lst[:]:
+#     if x.startswith('1'):
+#         lst.remove(x)
+# # print(lst)
+#
+# # 字典删除，循环字典删除，删除数据的数据会报错
+#
+# dic = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+# # for x in dic:  # dictionary changed size during iteration
+# #     dic.pop(x)
+# # 把要删除的键值放到一个列表中，循环删除
+# lst = list(dic.keys())
+# for x in lst:
 #     dic.pop(x)
-# 把要删除的键值放到一个列表中，循环删除
-lst = list(dic.keys())
-for x in lst:
-    dic.pop(x)
-# print(dic)
+# # print(dic)
+#
+# a = [10, 20, 30, 40, 50, 60, 70, 80]
+# b = [10, 20, 30, 40, 50, 60, 70, 80]
+#
+# print(a == b)  # True ,判断两个内容的值是否一致
+# print(a is b)  # False,判断两个内容的内存地址是否一致
+#
+# # 一般用is来判空
+# c = []
+# if c:
+#     print('c is None')
+# else:
+#     print('c is not None')
+# # c is not None 条件 if c 检查变量 c 是否为“真”。对于列表，非空列表被认为是“真”，而空列表被认为是“假”。
+#
+# d = []
+# if d is None:
+#     print('d is None')
+# else:
+#     print('d is not None')
+# # d is not None 条件 if d is None 检查变量 d 是否为 None。然而，一个空列表 ([]) 不是 None，它是一个空容器。因此，在你的代码中，d 不是 None。
+#
+# str2 = '我的家'
+# str1 = str2.replace('我','你')
+# print(str1)
+#
+# # https://tool.chinaz.com/regex 正则表达式 测试
+# lst = [1,2,3]
+# m = map(lambda x:x*2,lst)
+# print(list(m))
 
-a = [10, 20, 30, 40, 50, 60, 70, 80]
-b = [10, 20, 30, 40, 50, 60, 70, 80]
+lst = [{'1':'w'}, {'2':"w"}, {'1':"w"}]
+x =   [dict(y) for y in   {tuple(x.items()) for x in lst}]
+print(x)
 
-print(a == b)  # True ,判断两个内容的值是否一致
-print(a is b)  # False,判断两个内容的内存地址是否一致
-
-# 一般用is来判空
-c = []
-if c:
-    print('c is None')
-else:
-    print('c is not None')
-# c is not None 条件 if c 检查变量 c 是否为“真”。对于列表，非空列表被认为是“真”，而空列表被认为是“假”。
-
-d = []
-if d is None:
-    print('d is None')
-else:
-    print('d is not None')
-# d is not None 条件 if d is None 检查变量 d 是否为 None。然而，一个空列表 ([]) 不是 None，它是一个空容器。因此，在你的代码中，d 不是 None。
-
-str2 = '我的家'
-str1 = str2.replace('我','你')
-print(str1)
-
-# https://tool.chinaz.com/regex 正则表达式 测试
-lst = [1,2,3]
-m = map(lambda x:x*2,lst)
-print(list(m))
 
