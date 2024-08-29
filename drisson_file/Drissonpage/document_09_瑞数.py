aac18904d1043669de200e026ab794dc6edf4898
demon_home <762853138@qq.com>
2024-08-29 23:05:22 +0800

    all

--- drisson_file/Drissonpage/document_09_瑞数.py
+++ drisson_file/Drissonpage/document_09_瑞数.py
@@ -1,12 +1,12 @@
-from DrissionPage import ChromiumPage, ChromiumOptions
-from lxml import etree
-import re
-# 配置 ChromiumOptions
-co = ChromiumOptions()
-co.headless(False)  # 不使用无头模式
-co.incognito(True)  # 使用无痕模式
-
-page = ChromiumPage(co)
+# from DrissionPage import ChromiumPage, ChromiumOptions
+# from lxml import etree
+# import re
+# # 配置 ChromiumOptions
+# co = ChromiumOptions()
+# co.headless(False)  # 不使用无头模式
+# co.incognito(True)  # 使用无痕模式
+#
+# page = ChromiumPage(co)
 
 # 过瑞数mvp反爬 1、渲染方式 拿到html 2、监听数据包 3、接口的方式（只读取cookies）
 # 方法1
@@ -25,12 +25,12 @@ page = ChromiumPage(co)
 #     print(area,company,problem)
 
 #方法二：监听数据包
-page.listen.start('jsp/zdsswfaj/wwquery')
-page.get('http://beijing.chinatax.gov.cn/bjsat/office/jsp/zdsswfaj/wwquery.jsp')
-page('tx=东城').click()
-page.listen.wait()
-previous_data = []  # 用于存储前一页的数据
-n=1
+# page.listen.start('jsp/zdsswfaj/wwquery')
+# page.get('http://beijing.chinatax.gov.cn/bjsat/office/jsp/zdsswfaj/wwquery.jsp')
+# page('tx=东城').click()
+# page.listen.wait()
+# previous_data = []  # 用于存储前一页的数据
+# n=1
 # # 判断最后的数据跟之前是否相同而终止
 # for packet in page.listen.steps():              # 会监听wwquery.jsp 和 wwquery 2个数据包，一个是GET请求，一个是POST请求。
 #     # 此方法返回一个可迭代对象，用于for循环，每次循环可从中获取到的数据包。
@@ -62,29 +62,77 @@ n=1
 #     elif packet.method == 'GET':
 #         print('不需要')
 # page.close()
-for packet in page.listen.steps():              # 会监听wwquery.jsp 和 wwquery 2个数据包，一个是GET请求，一个是POST请求。
-    # 此方法返回一个可迭代对象，用于for循环，每次循环可从中获取到的数据包。
-    if packet.method == 'POST':
-        res_text = packet.response.body
-        iframe = etree.HTML(res_text)
-        tr_list = iframe.xpath('//tbody//td/table[2]//tr')[:-1]
-        page_max = page('tx:查询结果').text
-        num = re.search(r'/(?P<max>.*)',page_max)
-        # print(num.group('max'))
-        num = int(num.group('max'))
-        print(f"第{n}页")
-        for tr in tr_list:
-            area = tr.xpath('./td[1]/text()')[0]
-            company = tr.xpath('./td[2]/text()')[0]
-            problem = tr.xpath('./td[4]/text()')[0]
-            print(area,company,problem)
-        # 更新 previous_tr_list
-        if n == num :
-            print('到最后一页，结束。。。。。。。')
+# for packet in page.listen.steps():              # 会监听wwquery.jsp 和 wwquery 2个数据包，一个是GET请求，一个是POST请求。
+#     # 此方法返回一个可迭代对象，用于for循环，每次循环可从中获取到的数据包。
+#     if packet.method == 'POST':
+#         res_text = packet.response.body
+#         iframe = etree.HTML(res_text)
+#         tr_list = iframe.xpath('//tbody//td/table[2]//tr')[:-1]
+#         page_max = page('tx:查询结果').text
+#         num = re.search(r'/(?P<max>.*)',page_max)
+#         # print(num.group('max'))
+#         num = int(num.group('max'))
+#         print(f"第{n}页")
+#         for tr in tr_list:
+#             area = tr.xpath('./td[1]/text()')[0]
+#             company = tr.xpath('./td[2]/text()')[0]
+#             problem = tr.xpath('./td[4]/text()')[0]
+#             print(area,company,problem)
+#         # 更新 previous_tr_list
+#         if n == num :
+#             print('到最后一页，结束。。。。。。。')
+#             break
+#         n += 1
+#         page('下一页').click()
+#     elif packet.method == 'GET':
+#         print('不需要')
+# page.close()
+
+# 方法三：
+from DrissionPage import WebPage, ChromiumOptions
+import requests
+from lxml import etree
+
+co = ChromiumOptions()
+co.headless(False)  # 不使用无头模式
+co.incognito(True)  # 使用无痕模式
+page = WebPage('d', chromium_options=co)
+page.get('http://beijing.chinatax.gov.cn/bjsat/office/jsp/zdsswfaj/wwquery.jsp', retry=3, interval=2, timeout=15)
+# print('======>cookies', page.cookies(as_dict=True))
+browser_cookies = page.cookies(as_dict=True)
+print('======>VIP9lLgDcAL2T', browser_cookies['VIP9lLgDcAL2T'])
+# print('======>VIP9lLgDcAL2S',browser_cookies['VIP9lLgDcAL2S'])   #瑞数反爬
+# page.refresh()
+# browser_cookies = page.cookies(as_dict=True)
+# print('======>VIP9lLgDcAL2T', browser_cookies['VIP9lLgDcAL2T'])  #网页刷新/点击/超时都会刷新cookies
+# print('======>VIP9lLgDcAL2S',browser_cookies['VIP9lLgDcAL2S'])
+
+for pages in range(1,11):
+    url = 'http://beijing.chinatax.gov.cn/bjsat/office/jsp/zdsswfaj/wwquery'
+    headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+               "Cookie": f"VIP9lLgDcAL2T={browser_cookies['VIP9lLgDcAL2T']}; VIP9lLgDcAL2S={browser_cookies['VIP9lLgDcAL2S']};"}
+    data = {"orgCode": "11100000000",
+            "bz": "dq",
+            "dq":"东城",
+            "dqy": f"{pages}"}
+    response = requests.post(url=url,headers=headers,data=data)  #cookies 会失效
+    print(response.status_code)
+    for i in range(3):
+        response = requests.post(url=url, headers=headers, data=data)
+        print('状态码',response.status_code)
+        if response.status_code != 200:
+            page.refresh()
+            browser_cookies = page.cookies(as_dict=True)
+            headers['Cookie'] = f"VIP9lLgDcAL2T={browser_cookies['VIP9lLgDcAL2T']}; VIP9lLgDcAL2S={browser_cookies['VIP9lLgDcAL2S']};"
+            continue
+        else:
             break
-        n += 1
-        page('下一页').click()
-    elif packet.method == 'GET':
-        print('不需要')
-page.close()
+    print(f'第{pages}页')
+    tree = etree.HTML(response.text)
+    tr_list = tree.xpath('//tbody//td/table[2]//tr')[:-1]
+    for tr in tr_list:
+        area = tr.xpath('./td[1]/text()')[0]
+        company = tr.xpath('./td[2]/text()')[0]
+        problem = tr.xpath('./td[4]/text()')[0]
+        print(area,company,problem)
 
